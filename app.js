fb534b23f5e49b18231ccfdb9fbcb06c4e8258d8
Harsh <harshjha8453@gmail.com>
2025-07-24 13:16:07 +0530

    refactor: restructure project to MVC architecture, add user endpoints

--- app.js
+++ app.js
@@ -1,118 +1,21 @@
 import express from 'express';
-import fs from 'fs';
 import morgan from 'morgan';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import tourRouter from './routes/toursRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
-);
-
 app.use((req, res, next) => {
   console.log('Middleware ✅');
   next();
 });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    time: req.requestTime,
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-const getTourById = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No tour found with that ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-const updateTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No tour found with that ID',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tours >',
-    },
-  });
-};
-const deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'No tour found with that ID',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTourById)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}...`);
-});
+export default app;
---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+export const getAllUsers = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This Route is not yet defined' });
+};
+export const createUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This Route is not yet defined' });
+};
+export const getUserById = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This Route is not yet defined' });
+};
+export const updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This Route is not yet defined' });
+};
+export const deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This Route is not yet defined' });
+};
