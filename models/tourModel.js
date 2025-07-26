a22aef7b7d439294171773e0e622bfbe4b5fa415
Harsh <harshjha8453@gmail.com>
2025-07-26 18:25:33 +0530

    feat: complete full CRUD operations and schema for Tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,55 @@ const toursSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour duration is required'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: true,
+    required: [true, 'Tour must have a price'],
   },
+  discountPrice: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
