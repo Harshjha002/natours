f0359a25f67cb63add517ecca47d375dae4d1af3
Harsh <harshjha8453@gmail.com>
2025-07-31 21:26:31 +0530

    feat: handle unhandled routes with custom error response

--- app.js
+++ app.js
@@ -14,17 +14,20 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('Middleware ✅');
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+app.all('/{*any}', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on the server`,
+  });
+});
 export default app;
