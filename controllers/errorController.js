73acfadae46f757fba2f493e4c850f5f7d837b68
Harsh <harshjha8453@gmail.com>
2025-08-02 14:24:31 +0530

    feat: add error handler for Mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,6 +36,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -53,7 +59,9 @@ const globalErrorHandler = (err, req, res, next) => {
     if (error.code === 11000) {
       error = handleDuplicateFieldsDB(error);
     }
-
+    if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
     sendErrorProd(error, res);
   }
 };
