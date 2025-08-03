e7972e8bce54b82567c337240f4fe47fb2a0fc98
Harsh <harshjha8453@gmail.com>
2025-08-03 18:35:38 +0530

    feat: add basic User model for authentication

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is require'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is require'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
